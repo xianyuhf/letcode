61f4ecfc93a8498bfc4081a9fd05c842822d195b
xianyuhf <xianyuhf@163.com>
2022-06-15 11:48:21 +0800

    three

---
+++ Solution3.hpp
@@ -0,0 +1,30 @@
+#ifndef SOLUTION3_HPP
+#define SOLUTION3_HPP
+#include "pch.h"
+#include <string>
+class Solution3
+{
+private:
+    /* data */
+public:
+    Solution3();
+    //(/* args */)
+    ~Solution3();
+public:
+    int lengthOfLongestSubstring(string s){
+        unordered_set<char> occ;
+        int n=s.size();
+        
+    }
+};
+
+Solution3::Solution3(/* args */)
+{
+}
+
+Solution3::~Solution3()
+{
+}
+
+
+#endif
--- pch.h
+++ pch.h
@@ -2,4 +2,6 @@
 #define PCH_H
 #include <vector>
 #include <algorithm>
+#include <string>
+#include <unordered_set>
 #endif
